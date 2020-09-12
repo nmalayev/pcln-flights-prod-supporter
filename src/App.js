194827d30b683c3432f619af60eb36884edbd2d9
Nison Malayev <nmalayev@gmail.com>
2020-09-12 15:34:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ThemeProvider, Heading, Divider, Flex } from 'pcln-design-system'
+import { Departure, Arrival } from 'pcln-icons'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <Flex justifyContent='space-around' alignItems='center'>
+          <Departure size={18} color='background.dark'/>
+          <Heading.h3 color='primary.base' mx={1}>Flights Prod Supporter</Heading.h3>
+          <Arrival size={18} color='background.dark'/>
+        </Flex>
+
+        <Divider />
+
+        
+      </div>
+    </ThemeProvider>
   );
 }
 
